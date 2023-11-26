4ad6d5b2ff07c8b0512a49d9fdaab5856ecba8c9
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2023-11-26 18:32:18 +0900

    登録されていない県庁所在地が検索された場合、見つかりません。とういうメッセージを表示させる。

--- src/main/java/com/iwatsuki/Main.java
+++ src/main/java/com/iwatsuki/Main.java
@@ -10,7 +10,31 @@ public class Main {
         prefectureCapitals.put("青森県", "青森");
         prefectureCapitals.put("岩手県", "盛岡");
 
-        System.out.println(prefectureCapitals.get("青森県"));
+        String hokkaidoCapital = prefectureCapitals.get("北海道");
+        System.out.println("北海道の県庁所在地は" + hokkaidoCapital + "です");
 
+        try {
+            String prefecture = "東京";
+            String capital = getCapital(prefecture, prefectureCapitals);
+            System.out.println(prefecture + "の県庁所在地は" + capital);
+        } catch (NotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    static String getCapital(String prefecture, Map<String, String> map) throws NotFoundException {
+        String capital = map.get(prefecture);
+
+        if (capital == null) {
+            throw new NotFoundException(prefecture + "の県庁所在地は見つかりません");
+        }
+
+        return capital;
+    }
+
+    static class NotFoundException extends Exception {
+        public NotFoundException(String message) {
+            super(message);
+        }
     }
 }
